82f3cb9519207fe4730d46bb06cd55e8e1b8bca2
Ellie <j.chen.ellie@gmail.com>
2019-03-10 19:03:01 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,72 @@
+var button;
+
+//Time constructor
+function time(hour, minute) {
+    this.hour = hour;
+    this.minute = minute;
+}
+
+function extractTimeValues(timeStringValue) {
+    var hour = "", minute = "";
+    var i = 0;
+    
+    while (timeStringValue[i] != ':') {
+        hour += timeStringValue[i];
+        i++;
+    }
+    
+    //Increment i to avoid taking ':'
+    i++;
+    
+    while (i < timeStringValue.length) {
+        minute += timeStringValue[i];
+        i++;
+    }
+    
+    hour = parseInt(hour);
+    minute = parseInt(minute);
+    
+    var extractedTime = new time(hour, minute);
+    return extractedTime;
+}
+
+function buttonOperation() {
+    try {
+        primaryButtonOperation();
+    } catch (e) {
+        alert(e);
+    }
+}
+
+
+//Prepares data for actual calculation
+function primaryButtonOperation() {
+    var time1, time2;
+    
+    //Fetch time1 and time2
+    time1 = document.getElementsByClassName("time-field")[0].value;
+    time2 = document.getElementsByClassName("time-field")[1].value;
+    
+    //Input validation
+    if (time1 == "" || time2 == "") {
+        throw 'Time should not be empty';
+    }
+    
+    //Change to time type
+    time1 = extractTimeValues(time1);
+    console.log(time1);
+    //time2 = extractTimeValues(time2);
+    
+    //Calculation if passed
+    //calculateResult();
+    
+}
+
+//Actual calculation
+//function calculateResult(time1, time2) {
+//}
+
+
+button = document.getElementById("calculate");
+button.addEventListener("click", buttonOperation());
+
